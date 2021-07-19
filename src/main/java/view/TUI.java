03ad67b29a415a599475643d4372ab38d8b47556
jacob <jacobriis86@hotmail.com>
2021-07-19 13:02:23 +0200

    Changed TUI methods to be only onw SOUT instead of many

--- src/main/java/view/TUI.java
+++ src/main/java/view/TUI.java
@@ -1,18 +1,26 @@
 package view;
 
+import model.Player;
+
 public class TUI {
 
+    /**
+     * The first that will load when starting the game
+     *
+     * @throws InterruptedException because of sleep function
+     */
     public static void startup() throws InterruptedException {
 
-        System.out.println("-------------------------------------------------------------------------------------------");
-        System.out.println("|                                                                                         |");
-        System.out.println("|                                                                                         |");
-        System.out.println("|                                  WELCOME TO MATADOR                                     |");
-        System.out.println("|                                                                                         |");
-        System.out.println("|                         BECOME THE ULTIMATE MONOPOLY IN TOWN                            |");
-        System.out.println("|                                                                                         |");
-        System.out.println("|                                                                                         |");
-        System.out.println("-------------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------------\n" +
+                           "|                                                                                         |\n" +
+                           "|                                                                                         |\n" +
+                           "|                                  WELCOME TO MATADOR                                     |\n" +
+                           "|                                                                                         |\n" +
+                           "|                         BECOME THE ULTIMATE MONOPOLY IN TOWN                            |\n" +
+                           "|                                                                                         |\n" +
+                           "|                                                                                         |\n" +
+                           "-------------------------------------------------------------------------------------------");
+
         Thread.sleep(500);
         System.out.println("\n");
 
@@ -21,53 +29,46 @@ public class TUI {
             Thread.sleep(500);
         }
         System.out.println("                                   Loading: 100 %\n");
-        System.out.println("                                   Starting main menu");
+        System.out.println("                                   Starting main menu!\n");
 
     }
 
     /**
      * The main menu
-     *
-     * It will only
      */
     public static void mainMenu(){
-        System.out.println("-------------------------------------------------------------------------------------------");
-        System.out.println("|                                                                                         |");
-        System.out.println("|                                    Main Menu                                            |");
-        System.out.println("|                                                                                         |");
-        System.out.println("|                                                                                         |");
-        System.out.println("|                           Press '1' to start a new game!                                |");
-        System.out.println("|                           Press '2' to load a saved game!                               |");
-        System.out.println("|                                                                                         |");
-        System.out.println("|                                                                                         |");
-        System.out.println("-------------------------------------------------------------------------------------------");
-
-
+        System.out.println("-------------------------------------------------------------------------------------------\n" +
+                           "|                                                                                         |\n" +
+                           "|                                    Main Menu                                            |\n" +
+                           "|                                                                                         |\n" +
+                           "|                           Press '1' to start a new game!                                |\n" +
+                           "|                           Press '2' to load a saved game!                               |\n" +
+                           "|                                                                                         |\n" +
+                           "-------------------------------------------------------------------------------------------\n");
     }
 
     /**
      * Choose how many players you want to play
      */
     public static void preGameMenu(){
-        System.out.println("-------------------------------------------------------------------------------------------");
-        System.out.println("|                                                                                         |");
-        System.out.println("|               Input how many players there will be in the game!                         |");
-        System.out.println("|                                                                                         |");
-        System.out.println("|               To go back to the main menu type 'back'!                                  |");
-        System.out.println("|               To quit the game press 'quit'!                                            |");
-        System.out.println("|                                                                                         |");
-        System.out.println("-------------------------------------------------------------------------------------------");
-
+        System.out.println("-------------------------------------------------------------------------------------------\n" +
+                           "|                                                                                         |\n" +
+                           "|                  Input how many players there will be in the game!                      |\n" +
+                           "|                                                                                         |\n" +
+                           "|                  To go back to the main menu type 'back'!                               |\n" +
+                           "|                  To quit the game press 'quit'!                                         |\n" +
+                           "|                                                                                         |\n" +
+                           "-------------------------------------------------------------------------------------------\n");
     }
 
     /**
      * This will display a menu where the player can:
      *  - Check his properties
-     *  - Roll dice
-     *  - Buy houses
+     *  - Roll dice and take turn
+     *  - Buy houses or hotels
      *  -
      */
-    public static void turnMenu(){
+    public static void turnMenu(Player player){
 
     }
 
